9589d0f16c3b9a4bdda1d36e137fba2651259782
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-17 10:24:16 +0530

    added middleware for authorization

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,15 @@
 "use client"
-
+import {useEffect, useState} from 'react'
 import axios from 'axios';
 import {toast} from 'react-hot-toast'
 import {useRouter} from 'next/navigation'
+ 
 
 function Profile() {
 
   const router = useRouter()
+  const [user, setUser] = useState(null)
+  
 
   const onLogout = async () => {
     try {
@@ -18,9 +21,29 @@ function Profile() {
     }
   };
 
+  useEffect(()=> {
+     (async()=> {
+      try {
+        const response = await axios.get('/api/users/me')
+        if(response.data.success) {
+            toast.success(response.data.message)
+            setUser(response.data.data.user)
+        }
+      } catch (error:any) {
+        toast.error(error.message)
+      }
+      
+
+     })()
+  },[])
+
   return (
     <div>
-      <h1 className='text-center text-lg mt-5'>Profile</h1>
+      <h1 className='text-center text-lg mt-5 text-black'>Profile</h1>
+      <ul className='flex flex-col justify-center items-center my-3'>
+        <li>{user?.username}</li>
+        <li>{user?.email}</li>
+      </ul>
       <button onClick={onLogout} className='px-4 py-1 bg-green-500 rounded-md block mx-auto mt-2 text-white'>Logout</button>
     </div>
   );
